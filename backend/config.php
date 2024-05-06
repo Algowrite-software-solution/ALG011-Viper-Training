ef6e3c69430900e939b986ffab799e937980141b
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2024-05-07 00:41:01 +0530

    update server URL

--- backend/config.php
+++ backend/config.php
@@ -1,14 +1,14 @@
 <?php
 // server config
-define("ROOT", "http://localhost:9001/");
+define("ROOT", "https://portal.algowrite.com/");
 define("SESSION_VARIABLE_ADMIN", "alg011_admin");
 define("SESSION_VARIABLE_USER", "alg011_user");
 
 // database config
 define("DB_SERVER", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "******");
-define("DB_DATABASE", "alg011_db");
+define("DB_USERNAME", "algowrit_alg_user");
+define("DB_PASSWORD", "C).]%J9!%.[[");
+define("DB_DATABASE", "algowrit_alg011_db");
 
 //constants
 global $IS_RESPONSE_TEXT;
