5f6b393aa3aca29ad1750770e70182c3e7cac173
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-05-07 07:57:27 +0530

    ALG011-FED-T002.5
    finalized interactions

--- public/view/interface/register.php
+++ public/view/interface/register.php
@@ -22,13 +22,13 @@
                     <a href="#" class="forgot-password alg-text-dark-100">Forget your password?</a>
                     <button type="submit" class="register-button alg-bg-primary-dark alg-text-light-100 alg-bold alg-rounded-large">Register</button>
                 </form>
-                <p>Already have an account? <a href="/login" class="login-link alg-text-primary-dark">Sign in</a></p>
+                <p>Already have an account? <a href="/login" class="alg-text-primary-dark">Sign in</a></p>
                 <div class="divider alg-text-light-75">
                     <div class="line alg-bg-light-75"></div>
                     <span class="or">OR</span>
                     <div class="line alg-bg-light-75"></div>
                 </div>
-                <p>Request Admin Access: <a href="/request-admin-access" class="admin-access-link alg-text-primary-dark">Here</a></p>      
+                <p>Request Admin Access: <a href="/request-admin-access" class="alg-text-primary-dark">Here</a></p>      
             </div>
         </div>
     </div>
