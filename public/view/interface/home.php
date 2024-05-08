2da512d2ae47ff683cb19d210290d22151effde3
Niman Wijerathna <nimanwijerathna@gmail.com>
2024-05-08 22:42:54 +0530

    ALG011-FED-T004.1 update
    Updated some fields and the names of them

--- public/view/interface/home.php
+++ public/view/interface/home.php
@@ -2,25 +2,27 @@
 <!-- author : Niman Wijerathna
      created : 2024/05/02
      Description : this is the Home Page Layout-->
-<!-- home section (hs) -->
-<section class="hs-s1-container">
-    <div class="hs-s1-sidebar">
-        <div class="hs-s1-logo">
+<!-- home page (hp) -->
+<section class="hp-s1-container">
+</div>
+    <div class="hp-s1-sidebar">
+        <div class="hp-s1-logo">
         <div class="icon"></div>
-
             Algowrite
         </div>
-        <ul class="hs-s1-menu">
-            <li><a href="#" class="active"  onclick="showContent('dashboard')"><img src="../../resources/images/home/category.png" alt="Dashboard Icon">Dashboard</a></li>
-            <li><a href="#" onclick="showContent('user')"><img src="../../resources/images/home/2-user.png" alt="User Icon">User Management</a></li>
-            <li><a href="#" onclick="showContent('project')"><img src="../../resources/images/home/bag.png" alt="Bag Icon"> Project Management</a></li>
+       
+        <ul class="hp-s1-menu">
+            <li><a href="#" class="active"><img src="../../resources/images/home/category.png" alt="Dashboard Icon">Dashboard</a></li>
+            <li><a href="#"><img src="../../resources/images/home/2-user.png" alt="User Icon">User Management</a></li>
+            <li><a href="#"><img src="../../resources/images/home/bag.png" alt="Bag Icon"> Project Management</a></li>
         </ul>
     </div>
-    <div class="hs-s1-content">
-        <div class="hs-s1-profile">
-        <div class="hs-s1-title">Product</div>
-        <button class="hs-s1-btn">Sign Out</button>
-            <div class="hs-s1-user-icon">JN</div>
+    <div class="hp-s1-content">
+        <div class="hp-s1-profile">
+        
+        <div class="hp-s1-title">Product</div>
+        <button class="hp-s1-btn">Sign Out</button>
+            <div class="hp-s1-user-icon">JN</div>
            
         </div>
     </div>
