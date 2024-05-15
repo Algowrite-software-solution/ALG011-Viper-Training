a52b8828c9f32d4191cb2fb75b183b77ae1ea7e1
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-05-15 13:41:09 +0530

    ALG011-FED-T001.6

--- public/view/interface/signin.php
+++ public/view/interface/signin.php
@@ -1,41 +1,43 @@
 <!-- Signin Page (sig) -->
-<section>
-    <div class="sig-container">
-       <div class="sig-imageside">
-            <div class="sig-imagesidetxt">
-                <div class="sig-circle"></div>
-                <h1><b>Algowrite</b></h1>
-                <h5>Unleashing the magic of technology...!</h5>
+<section class="vh-100">
+    <div class="col-12 h-100">
+        <div class="d-flex flex-column flex-md-row m-0  h-100">
+            <div class="sig-left-panel col-12 col-md-6 bg-primary d-flex flex-column justify-content-center p-5 alg-text-light gap-2 ">
+                <div class="sig-dot"></div>
+                <h1>Algowrite</h1>
+                <p>Unleashing the magic of technology...!</p>
             </div>
-            
-            <img src="../../resources/images/signin/signinimage.png" alt="signinimage" class="sig-image">
-       </div>
 
-       <div class="sig-side">      
-            <form action="/" method="post">
-                <p class="sig-txt">Sign In</p>
-                <div class="sig-txtbox">
-                    <label for="Email">Email address</label><br>
-                    <input type="email" class="sig-inputbox" id="Email" placeholder="Enter your account email..." required>
+            <div class="sig-right-panel gap-4 col-12 col-md-6 py-5 d-flex flex-column justify-content-center align-items-center ">
+                <h2>Sign In</h2>
+                <div class="gap-4 w-100 px-5">
+                    <form action="/" method="post"> 
+                            <div class="alg-text-primary fw-bold px-4">
+                                    <label class="ps-4" for="Email">Email address</label><br>
+                                    <input type="email" class="alg-input" id="Email" placeholder="Enter your account email..." required>
+                            </div>
+                            <br>
+                            <div class="alg-text-primary fw-bold px-4">
+                                    <label class="ps-4" for="Password">Password</label><br>
+                                    <input type="password" class="alg-input" id="Password" placeholder="Create a strong password..." required>
+                            </div>            
+                            <div class="d-flex flex-column justify-content-center align-items-center ">
+                                <a href="#" class="alg-text-dark-100">Forget your password?</a><br>
+                                <button type="submit" class="col-6 col-md-5 col-lg-4 alg-btn">Sign in</button>
+                            </div> 
+                    </form>
+                    <div class="d-flex flex-column justify-content-center align-items-center ">
+                        <p class="mt-3 alg-text-light-50">Don’t have an account?  <a href="/register" class="alg-text-primary-dark">Register</a></p>
+                        <div class="sig-line-main alg-text-light-75">
+                            <div class="sig-line alg-bg-light-75"></div>
+                            <span class="px-4">OR</span>
+                            <div class="sig-line alg-bg-light-75"></div>
+                        </div>
+                        <br>
+                        <p class="alg-text-light-50">Request Admin Access: <a href="/" class="alg-text-primary-dark">Here</a></p>      
+                    </div> 
                 </div>
-                <br>
-                <div class="sig-txtbox">
-                    <label for="Password">Password</label><br>
-                    <input type="password" class="sig-inputbox" id="Password" placeholder="Create a strong password..." required>
-                </div>  
-                <div class="sig-centertxt">
-                    <a href="#" class="sig-forgotpassword">Forget your password</a><br>           
-                    <button type="submit" class="sig-submitbtn">Sign in</button><br><br>
-                    <span class="sig-graytxt">Don't have an account? </span><a href="/register" >Register</a><br><br>
-                    <div class="sig-linediv">
-                        <hr class="sig-line">
-                        <span class="sig-graytxtline">OR</span>
-                        <hr class="sig-line">
-                    </div>            
-                    <br>
-                    <span class="sig-graytxt">Request Admin Access </span><a href="#" >Here</a> 
-                </div>
-            </form>
-       </div>    
+            </div>
+        </div>
     </div>
 </section>
